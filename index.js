6ee00d2c2e70dba838d85133c725a81014d1f497
Alam <alamkhalid0803@gmail.com>
2021-05-27 17:42:47 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const graphQLSchema = require("./graphql/schema/index");
+const graphQLResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middleware/is-auth");
+
+const app = express();
+app.use(isAuth);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphQLSchema,
+    rootValue: graphQLResolvers,
+    graphiql: true,
+  })
+);
+
+// Connect to database
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9ym60.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`Server started at ${PORT}...`);
+    });
+  })
+  .catch((err) => console.log(err));
